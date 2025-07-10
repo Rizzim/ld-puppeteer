e653f84ad999ac0c8871563ce51f15dc1ba9a1a1
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2025-07-10 16:22:16 +0500

    Updated puppeteer config to headless

--- config/puppeteerConfig.js
+++ config/puppeteerConfig.js
@@ -1,11 +1,11 @@
 export const puppeteerOptions = {
-    headless: false ,  // Mark headless as false for debugging (UI will be visible)
+    headless: true ,  // Mark headless as false for debugging (UI will be visible)
     args: [
         '--disable-web-security',
         '--disable-features=IsolateOrigins,site-per-process',
         '--disable-gpu',
         '--no-sandbox',
-        // '--headless=old', // Also comment this when debugging
+        '--headless=old', // Also comment this when debugging
         '--disable-setuid-sandbox',
         '--window-size=1920,1080',
         '--no-startup-window', '--no-first-run'
