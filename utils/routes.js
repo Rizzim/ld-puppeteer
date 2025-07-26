db8d18b18dc67be4b0afc106c61a3dcc669d3177
Anis Momin <anis_momin@hotmail.com>
2025-07-26 14:48:34 +0500

    Update: home route added

--- utils/routes.js
+++ utils/routes.js
@@ -4,6 +4,10 @@ import errorHandler from '../middlewares/errorHandler.js';
 
 export default function (app){
     app.use(json());
+    // Home route
+    app.get('/', (req, res) => {
+        res.json({ status: 'ok', message: 'Puppeteer Sales Automation is running!' });
+    });
     app.use("/api/puppeteer", puppeteerRoutes);
     app.use(errorHandler);
 }
